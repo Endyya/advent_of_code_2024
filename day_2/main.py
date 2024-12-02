f6e9aa57f4611fd4816af9e1b53ca8d605d9c837
Endyya <endya@protonmail.com>
2024-12-02 21:16:35 +0100

    Flexing for kepotx

--- day_2/main.py
+++ day_2/main.py
@@ -34,4 +34,6 @@ final_check = np.logical_and(
 
 print('part 1 :', np.sum(final_check))
 
+#### Flexing with one line answer for part 1 :
+print('part 1 :', np.sum(np.logical_and(np.logical_or(np.prod(np.greater_equal(0, data[:, :-1] - data[:, 1:], out = np.ones(shape = (data.shape[0], data.shape[1] - 1), dtype = bool), where = ~np.isnan(data[:, :-1] - data[:, 1:])), axis = 1, dtype = bool), np.prod(np.greater_equal(data[:, :-1] - data[:, 1:], 0, out = np.ones((data.shape[0], data.shape[1] - 1), dtype = bool), where = ~np.isnan(data[:, :-1] - data[:, 1:])), axis = 1, dtype = bool)), np.prod(np.logical_and(np.greater_equal(3, np.abs(data[:, :-1] - data[:, 1:]), out = np.ones((data.shape[0], data.shape[1] - 1), dtype = bool), where = ~np.isnan(data[:, :-1] - data[:, 1:])), np.greater_equal(np.abs(data[:, :-1] - data[:, 1:]), 1, out = np.ones((data.shape[0], data.shape[1] - 1), dtype = bool), where = ~np.isnan(data[:, :-1] - data[:, 1:]))), axis = 1, dtype = bool))))
 
