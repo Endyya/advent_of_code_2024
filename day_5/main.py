25fcad3844a915616313ee870cd3741c7d43ec12
Endyya <endya@protonmail.com>
2024-12-05 12:10:01 +0100

    Cleaned main day 5

--- day_5/main.py
+++ day_5/main.py
@@ -22,20 +22,7 @@ with open('input') as f:
             updates.append(line.split(','))
 
 order = np.array(order, dtype = int)
-order = order[order[:, 0].argsort()]
 updates = [[int(i) for i in line] for line in updates]
-
-score_1 = 0
-
-
-for update in updates:
-    if check_one_update(update, order):
-        score_1 += update[len(update) // 2]
-
-to_sort = list(set(order[:, 0]).union(set(order[:, 1])))
-
-print('part 1 :', score_1)
-
 
 my_graph = nx.DiGraph()
 
@@ -43,33 +30,7 @@ all_values = set(order[:, 0]).union(set(order[:, 1]))
 my_graph.add_nodes_from(all_values)
 for order_node in order:
     my_graph.add_edge(order_node[1], order_node[0])
-# for val_1, val_2, val_3 in itt.combinations(all_values, r = 3):
-#     if (my_graph.has_edge(val_1, val_2)
-#         and my_graph.has_edge(val_2, val_3)
-#         and my_graph.has_edge(val_1, val_3)):
-#         my_graph.remove_edge(val_1, val_3)
-#     if (my_graph.has_edge(val_1, val_3)
-#         and my_graph.has_edge(val_3, val_2)
-#         and my_graph.has_edge(val_1, val_2)):
-#         my_graph.remove_edge(val_1, val_2)
-#     if (my_graph.has_edge(val_2, val_3)
-#         and my_graph.has_edge(val_3, val_1)
-#         and my_graph.has_edge(val_2, val_1)):
-#         my_graph.remove_edge(val_2, val_1)
-#     if (my_graph.has_edge(val_2, val_1)
-#         and my_graph.has_edge(val_1, val_3)
-#         and my_graph.has_edge(val_2, val_3)):
-#         my_graph.remove_edge(val_2, val_3)
-#     if (my_graph.has_edge(val_3, val_1)
-#         and my_graph.has_edge(val_1, val_2)
-#         and my_graph.has_edge(val_3, val_2)):
-#         my_graph.remove_edge(val_3, val_2)
-#     if (my_graph.has_edge(val_3, val_2)
-#         and my_graph.has_edge(val_2, val_1)
-#         and my_graph.has_edge(val_3, val_1)):
-#         my_graph.remove_edge(val_3, val_1)
 
-        
 def sort_update(graph, update):
     if len(update) <= 1:
         return update[:]
@@ -78,19 +39,19 @@ def sort_update(graph, update):
     return (sort_update(graph, list(higher))
             + [update[0]]
             + sort_update(graph, list(lower)))
-score_2 = 0        
+score_1 = score_2 = 0        
         
 for update in updates:
     sort = sort_update(my_graph, update)
     if update != sort:
         score_2 += sort[len(sort) // 2]
     else:
-        print('bad :', update)
+        score_1 += sort[len(sort) // 2]
 
 
 
 
-
+print('part 1 :', score_1)
 print('part 2 :', score_2)
         
 
