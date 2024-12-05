d13782a1db324107e16f3ca6bcc8c1fbc3ae2a07
Endyya <endya@protonmail.com>
2024-12-05 12:08:00 +0100

    Day 5 ** Finished

--- day_5/main.py
+++ day_5/main.py
@@ -1,7 +1,10 @@
 import numpy as np
+import itertools as itt
+import networkx as nx
 order = []
 updates = []
 sorted_pages = []
+
 
 def check_one_update(update, order):
     for i, page in enumerate(update):
@@ -22,14 +25,73 @@ order = np.array(order, dtype = int)
 order = order[order[:, 0].argsort()]
 updates = [[int(i) for i in line] for line in updates]
 
-score = 0
+score_1 = 0
+
 
 for update in updates:
     if check_one_update(update, order):
-        score += update[len(update) // 2]
+        score_1 += update[len(update) // 2]
+
+to_sort = list(set(order[:, 0]).union(set(order[:, 1])))
+
+print('part 1 :', score_1)
 
 
-print('part 1 :', score)
+my_graph = nx.DiGraph()
+
+all_values = set(order[:, 0]).union(set(order[:, 1]))
+my_graph.add_nodes_from(all_values)
+for order_node in order:
+    my_graph.add_edge(order_node[1], order_node[0])
+# for val_1, val_2, val_3 in itt.combinations(all_values, r = 3):
+#     if (my_graph.has_edge(val_1, val_2)
+#         and my_graph.has_edge(val_2, val_3)
+#         and my_graph.has_edge(val_1, val_3)):
+#         my_graph.remove_edge(val_1, val_3)
+#     if (my_graph.has_edge(val_1, val_3)
+#         and my_graph.has_edge(val_3, val_2)
+#         and my_graph.has_edge(val_1, val_2)):
+#         my_graph.remove_edge(val_1, val_2)
+#     if (my_graph.has_edge(val_2, val_3)
+#         and my_graph.has_edge(val_3, val_1)
+#         and my_graph.has_edge(val_2, val_1)):
+#         my_graph.remove_edge(val_2, val_1)
+#     if (my_graph.has_edge(val_2, val_1)
+#         and my_graph.has_edge(val_1, val_3)
+#         and my_graph.has_edge(val_2, val_3)):
+#         my_graph.remove_edge(val_2, val_3)
+#     if (my_graph.has_edge(val_3, val_1)
+#         and my_graph.has_edge(val_1, val_2)
+#         and my_graph.has_edge(val_3, val_2)):
+#         my_graph.remove_edge(val_3, val_2)
+#     if (my_graph.has_edge(val_3, val_2)
+#         and my_graph.has_edge(val_2, val_1)
+#         and my_graph.has_edge(val_3, val_1)):
+#         my_graph.remove_edge(val_3, val_1)
+
+        
+def sort_update(graph, update):
+    if len(update) <= 1:
+        return update[:]
+    higher = set(graph.successors(update[0])).intersection(set(update))
+    lower = set(graph.predecessors(update[0])).intersection(set(update))
+    return (sort_update(graph, list(higher))
+            + [update[0]]
+            + sort_update(graph, list(lower)))
+score_2 = 0        
+        
+for update in updates:
+    sort = sort_update(my_graph, update)
+    if update != sort:
+        score_2 += sort[len(sort) // 2]
+    else:
+        print('bad :', update)
+
+
+
+
+
+print('part 2 :', score_2)
         
 
 
