ce69137de5710dcb1c2c51738de9a3537c92be1c
Endyya <endya@protonmail.com>
2024-12-07 18:43:45 +0100

    Day 7 * Finished

---
+++ day_7/main.py
@@ -0,0 +1,27 @@
+
+
+def parse(line):
+    number, elements = line.split(':')
+    number = int(number)
+    elements = elements.strip('\n ')
+    elements = elements.split(' ')
+    elements = [int(n) for n in elements]
+    return number, elements
+
+def is_good(n, lon):
+
+    if len(lon) > 1:
+        test_sum = is_good(n - lon[-1], lon[:-1])
+        test_prod = not n % lon[-1]  and is_good(n // lon[-1], lon[:-1])
+        return test_sum or test_prod
+    elif len(lon) == 1:
+        return lon[0] == n
+
+score = 0
+with open('input') as f:
+    for line in f:
+        n, lon = parse(line)
+        if is_good(n, lon):
+            score += n
+
+print('part 1 :', score)
