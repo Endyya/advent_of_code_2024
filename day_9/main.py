404cc6ad5c288a2aa290f60d92a4f67950d59faf
Endyya <endya@protonmail.com>
2024-12-14 23:47:28 +0100

    Day 9 ** Finished (broken part 1)

--- day_9/main.py
+++ day_9/main.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-
-
 with open('input') as f:
     line = f.readline().rstrip('\n')
 
@@ -11,25 +9,51 @@ empty_space = np.array(list(line)[1::2])
 data_box = np.zeros(sum([int(i) for i in line]), dtype = int)
 index = 0
 ID = 0
-
-for i, j in enumerate(line):
-    j = int(j)
-
-    if i % 2 == 0:
-        data_box[index:index + j] = ID
-        ID += 1
-        index = index + j
-    else:
-        data_box[index:index + j] = -1
-        index = index + j
-
-for i, ID in enumerate(data_box[::-1]):
-    first_empty = list(data_box).index(-1)
-    if ID != -1 and first_empty < len(data_box) - i - 1:
-        data_box[first_empty] = ID
-        data_box[-i-1] = -1
-
-first_empty = list(data_box).index(-1)
-print('part 1 :', sum([i*j for i,j in enumerate(data_box[:first_empty])]))
         
-        
+free_chunks_size = np.array(empty_space, dtype = int)
+free_chunks_indexes = np.array([sum([int(chunk) for chunk in line[:2*i+1]])
+                                for i in range(len(line) // 2)], dtype = int)
+
+data_chunks_size = np.array(true_data, dtype = int)
+data_chunks_indexes = np.array([sum([int(chunk) for chunk in line[:2*i]])
+                                for i in range(len(line) // 2 + 1)], dtype = int)
+IDs = np.array(range(len(data_chunks_indexes)))
+
+
+
+def make_data_box(data_size, data_ind, data_ID):
+    data_box = np.full(max(data_ind) + max(data_size), fill_value = 0, dtype = int)
+
+    for i, index in enumerate(data_ind):
+        data_box[index:index+data_size[i]] = data_ID[i]
+    return data_box
+
+data_box = make_data_box(data_chunks_size, data_chunks_indexes, IDs)
+
+print('broken part 1 :', sum([i*j for i,j in enumerate(data_box)]))
+
+
+# run on ID backwards
+for ID in IDs[::-1]:
+
+    # get chunk size and indexes for this ID
+    chunk_size = data_chunks_size[ID]
+    chunk_index = data_chunks_indexes[ID]
+
+    # find the lowest empty size for this chunk
+    try:
+        lowest_empty_size = free_chunks_size[
+            np.logical_and(free_chunks_size >= chunk_size,
+                           free_chunks_indexes <= chunk_index)][0]
+        lowest_empty_index = free_chunks_indexes[free_chunks_size >= chunk_size][0]
+    except IndexError:
+        continue
+
+    data_chunks_indexes[ID] = lowest_empty_index
+    free_chunks_indexes[list(free_chunks_size).index(lowest_empty_size)] += chunk_size    
+    free_chunks_size[list(free_chunks_size).index(lowest_empty_size)] -= chunk_size
+
+
+data_box = make_data_box(data_chunks_size, data_chunks_indexes, IDs)
+
+print('part 2 :', sum([i*j for i,j in enumerate(data_box)]))
